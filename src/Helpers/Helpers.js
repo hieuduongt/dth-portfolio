f7177a8f94fc631c9fd6fb2912c9049d3d729b9b
Hieu Duong Thanh <hieu.duong@efundamentals.com>
2023-11-25 17:42:14 +0700

    fix border, animation when resize window, add game iframe

--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -6,14 +6,13 @@ export const createHiddenStyle = (rect, width, height) => {
             fontSize: "0",
             opacity: 0,
             visibility: "hidden",
-            transition: "left 0.2s, right 0.2s linear, bottom 0.3s linear, transform 0.3s ease-in-out, translate 0.3s ease-in-out, visibility 0.4s ease-in-out, opacity 0.4s ease-in-out, width 0.2s ease-in-out, height 0.2s ease-in-out",
+            transition: "transform 0.3s ease-in-out, visibility 0.4s ease-in-out, opacity 0.4s ease-in-out, width 0.2s ease-in-out, height 0.2s ease-in-out",
             width: "50px !important",
             height: "50px !important",
-            left: `${rect.left + 50}px !important`,
-            top: `${rect.top}px !important`
+            transformOrigin: `${rect.left}px ${rect.top}px`
         },
         position: {
-            x: rect.left + 50,
+            x: rect.left,
             y: rect.top
         },
         size: {
@@ -29,19 +28,19 @@ export const createVisibleStyle = (rect, animation = true) => {
         style: {
             opacity: 1,
             visibility: "visible",
-            transition: animation ? "right 0.3s linear, left 0.3s linear, bottom 0.3s linear, transform 0.3s ease-in-out, translate 0.3s ease-in-out, visibility 0.4s ease-in-out, opacity 0.4s ease-in-out, width 0.2s ease-in-out, height 0.2s ease-in-out" : "",
+            transition: animation ? "right 0.3s linear, left 0.3s linear, bottom 0.3s linear, transform 0.3s ease-in-out, translate 0.3s ease-in-out, visibility 0.1s ease-in-out, opacity 0.1s ease-in-out, width 0.3s ease-in-out, height 0.3s ease-in-out" : "",
         },
         rect: rect
     };
 }
 
-export const handleOpenAnApplication = async (index, name, style, setStyle, setIsRunningApp, setAreOpenedApps, openedApps) => {
+export const handleOpenAnApplication = async (index, name, style, setStyle, setIsRunningApp, setAreOpenedApps, openedApps, rect) => {
     setIsRunningApp(index);
     setAreOpenedApps({ ...openedApps, [name]: 1 });
-    const newStyle = !style.style || style.style.visibility === 'hidden' ? createVisibleStyle(style.rect) : createHiddenStyle(style.rect, 50, 50);
+    const newStyle = !style.style || style.style.visibility === 'hidden' ? createVisibleStyle(rect || style.rect) : createHiddenStyle(rect || style.rect, 50, 50);
     setStyle(newStyle);
     if (newStyle.style.visibility === "visible") {
-        const styleWithNoAnimation = createVisibleStyle(style.rect, false);
+        const styleWithNoAnimation = createVisibleStyle(rect, false);
         await sleep(300);
         setStyle(styleWithNoAnimation);
     }
